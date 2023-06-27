a8b9e16ddc2651b42d23942a22d1a481415b07b1
Pherrows <yhk1492@naver.com>
2023-06-27 17:11:13 +0900

    2023 - 6 -27
    calendar

---
+++ src/components/calendar/CalendarHeder.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill} from 'react-icons/bs'
+import { format } from 'date-fns'
+import styled, {} from 'styled-components'
+
+
+const Heder = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid gray;
+`;
+
+const StyledDiv = styled.div`
+  .heder-start{
+    
+  }
+  .heder-end{
+
+  }
+
+  svg {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+`;
+
+const StyledSpan = styled.span`
+  .month {
+    font-size: 20px;
+    margin-left: 10px;
+    margin-right: 7px ;
+    font-weight: bold;
+  }
+  .year {
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
+
+
+
+function CalendarHeder({ currentMonth, prevMonth, nextMonth }) {
+  return (
+    <Heder className='heder'>
+      <StyledDiv className='heder-start'>
+        <StyledSpan className='year'>
+          <StyledSpan className='month'>
+            {format(currentMonth, 'M')}월
+          </StyledSpan>
+          {format(currentMonth, 'yyyy')}
+        </StyledSpan>
+      </StyledDiv>
+
+      <StyledDiv className='heder-end'>
+        <BsFillArrowLeftCircleFill className='left' size={18} color='grey' onClick={prevMonth}/>
+        <BsFillArrowRightCircleFill className='right' size={18} color='grey' onClick={nextMonth }/>
+      </StyledDiv>
+    </Heder>
+  );
+}
+
+export default CalendarHeder;
--- src/page/Calendar.js
+++ src/page/Calendar.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CalendarHeder from '../components/calendar/CalendarHeder';
+import { addMonths, subMonths, format } from 'date-fns';
+import CalendarDay from '../components/calendar/CalendarDay';
+import CalendarSells from '../components/calendar/CalendarSells';
 
 function Calendar(props) {
+
+  const [currentMonth, setcurrentMonth] = useState(new Date());
+
+  const prevMonth = () => {
+    setcurrentMonth(subMonths(currentMonth, 1))
+  };
+  const nextMonth = () => {
+    setcurrentMonth(addMonths(currentMonth, 1))
+  };
+
   return (
     <div className='show-content'>
-        <p> 캘린더 </p>
+      <>
+        <CalendarHeder currentMonth={currentMonth} prevMonth={prevMonth} nextMonth={nextMonth} />
+        <CalendarDay />
+        <CalendarSells currentMonth={currentMonth}/>
+      </>
     </div>
   );
 }

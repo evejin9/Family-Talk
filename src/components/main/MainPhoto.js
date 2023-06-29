2fab4340a661217aeed470472ceff2000cdfb482
rladbal0101 <rladbal0101@naver.com>
2023-06-29 11:26:04 +0900

    2023-06-29 Main
    - 메인사진 수정버튼 추가

--- src/components/main/MainPhoto.js
+++ src/components/main/MainPhoto.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import familyImg from "../../image/famsta5.jpg";
+import { MdEdit } from "react-icons/md";
 
-
+const MainPhotoStyled = styled.div`
+  position: relative;
+`;
 const StyledImg = styled.img`
+
   width: 100%;
   height: 500px;
   background: url(${familyImg});
@@ -13,12 +16,34 @@ const StyledImg = styled.img`
   background-position: center;
 `;
 
+const Edited = styled.div`
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+
+  svg {
+    font-size: 25px;
+    color: #ccc;
+    cursor: pointer;
+
+    &:hover {
+      color: #282c34;
+    }
+  }
+`;
+
 function MainPhoto(props) {
   return (
-    <>
-        <StyledImg />
-      
-    </>
+    <MainPhotoStyled>
+      <StyledImg />
+      <Edited
+        onClick={() => {
+          
+        }}
+      >
+        <MdEdit />
+      </Edited>
+    </MainPhotoStyled>
   );
 }
 
